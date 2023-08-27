e186197f93a694491f57f89a78d840ce49129b3b
aminaibrahimelhendy <amina24092007@gmail.com>
2023-08-27 14:34:16 +0300

    Update amina.py

--- amina.py
+++ amina.py
@@ -1,27 +1,28 @@
-my_name = input("what is your name?")
-my_age = int(input("how old are you?"))
-print(f"My name is {my_name}, and I am {my_age} years old")
+my_name = (input("what ia your name?"))
+my_age =int(input("how old are you?"))
+print(f"my name is {my_name} , and i am {my_age} years old")
 
-first_number = int(input("give me the first number")) 
-second_number = int(input("give me the second number"))
-operation = input("give me the operation you would like to make")
-if operation == '+':
-    print(first_number + second_number)
+first_number=int(input("give me the first number"))
+second_number=int(input("give me the second number"))
+operation=(input("give me the operation you would like to make"))
+if operation == "+":
+    print( first_number+second_number)
 elif operation == "-":
-    print(first_number - second_number)
+    print( first_number-second_number)
 elif operation == "*":
-    print(first_number * second_number)
+    print(first_number*second_number)
 elif operation == "/":
-    print(first_number / second_number) 
+    print(first_number/second_number)
 else:
-    print("Invalid operation")
+    print("the operation is not valid")
 
-bus_capacity = 24
-people_in_bus = int(input("how much people in the bus?"))
-empty_seats = bus_capacity - people_in_bus
-if empty_seats > 0:
-    print(f"There are {empty_seats} seats empty in the bus")
-else:
-    print("There are no empty seats!")
-
-   
+bus_capacity =24
+people_inbus =int(input("how many people in the bus?"))
+people_outside=int(input("how many people want to enter the bus?"))
+empty_seats = bus_capacity - people_inbus
+if empty_seats>people_outside:
+    print(f"there is {empty_seats} empty seats")
+elif empty_seats<people_outside or empty_seats==people_outside:
+    print("there is no empty seats")
+    
+    
